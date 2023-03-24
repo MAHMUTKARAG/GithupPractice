67a71cc5e3b80778cd1fd350df6c68d418a331a8
MAHMUTKARAG <mhmtkrgz24@gmail.com>
2023-03-24 15:39:35 -0700

    main change

--- src/main/java/Githup/Practice.java
+++ src/main/java/Githup/Practice.java
@@ -5,6 +5,8 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("That's it");
+        System.out.println("main branch change first comment");
+        System.out.println("second one");
     }
 
 
