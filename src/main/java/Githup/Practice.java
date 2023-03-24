c4d5183eece3641e3168e0d1e396fe25370b4200
MAHMUTKARAG <mhmtkrgz24@gmail.com>
2023-03-24 15:36:03 -0700

    second change of mahmut

--- src/main/java/Githup/Practice.java
+++ src/main/java/Githup/Practice.java
@@ -5,6 +5,7 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("That's it");
+        System.out.println("i did some change of public class ");
     }
 
 
--- src/main/java/Githup/mahmut.java
+++ src/main/java/Githup/mahmut.java
@@ -3,5 +3,6 @@ package Githup;
 public class mahmut {
     public static void main(String[] args) {
         System.out.println("working on mahmut branch");
+        System.out.println("second change on this class only");
     }
 }
